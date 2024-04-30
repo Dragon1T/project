e4587a09e5d30995e88a10ff750157faaeadc2e7
Andrey <andrey.chuprynin.2008@mail.ru>
2024-04-30 17:21:56 +0300

    project-commit 1

---
+++ script.js
@@ -0,0 +1,7 @@
+const tableElements = document.querySelectorAll('.table .table__element')
+
+for (let i = 0; i < tableElements.length; i++) {
+  tableElements[i].addEventListener('click', () => {
+    tableElements[i].innerHTML = i + 1
+  })
+}
